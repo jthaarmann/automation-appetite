a8e2a61404d2a70e1b79af5d859aa8b2289b2c25
Haarmann, Jonathan <Jonathan.Haarmann@ehi.com>
2017-10-09 09:14:44 -0500

    Made Day screen 2 cols for diet and exercise

--- dashboard/src/main/js/src/containers/Day.js
+++ dashboard/src/main/js/src/containers/Day.js
@@ -1,5 +1,5 @@
 import React, { Component } from 'react'
-import { Row, Grid } from 'react-bootstrap'
+import { Row, Col, Grid } from 'react-bootstrap'
 import DailyActivityDetails from '../components/DailyActivityDetails'
 import DailyExercises from '../components/DailyExercises'
 import DailyDiet from '../components/DailyDiet'
@@ -20,10 +20,12 @@ class Day extends Component {
 					<DailyActivityDetails dayAbbreviation={this.state.dayAbbreviation}/>
 				</Row>
 				<Row className="show-grid">
-					<DailyDiet dayAbbreviation={this.state.dayAbbreviation}/>
-				</Row>
-				<Row className="show-grid">
-					<DailyExercises dayAbbreviation={this.state.dayAbbreviation}/>
+					<Col md={6}>
+						<DailyDiet dayAbbreviation={this.state.dayAbbreviation}/>
+					</Col>
+					<Col md={6}>
+						<DailyExercises dayAbbreviation={this.state.dayAbbreviation}/>
+					</Col>
 				</Row>
 			</Grid>	
 		);
